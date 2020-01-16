8a9faedd205c6f5df89602a86e93989cb94e97a4
NathanLiew <59965730+NathanLiew@users.noreply.github.com>
2020-01-16 17:47:11 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -6,10 +6,24 @@ Nathan
 Purpose to collaborate with others
 
 */
+/**
+Nathan Liew
+Date: 1/16/2020
+Major: Computer Science
+Collaborators: John Cerny
+*/
+
 
 #include <stdio.h>
 
 int main(void) {
 	printf("John Cerny Mechanical Engineering\n");
+	return 0;
+}
+#include <stdio.h>
+
+int main(void) {
+	printf("Nathan Liew \n");
+	printf("Computer Science")
 	return 0;
 }
