c2d0f81cc43a97c8d589370f4fe90a182c44059d
88342998 <59965860+88342998@users.noreply.github.com>
2020-01-16 20:29:31 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -4,13 +4,18 @@ John Cerny
 1/16/2020
 Nathan
 Purpose to collaborate with others
-
-*/
 /**
+
+John Cerny
+1/16/2020
+Collaborators: Nathan Liew
+Purpose to collaborate with others
+
 Nathan Liew
 Date: 1/16/2020
 Major: Computer Science
 Collaborators: John Cerny
+
 */
 
 
@@ -18,12 +23,7 @@ Collaborators: John Cerny
 
 int main(void) {
 	printf("John Cerny Mechanical Engineering\n");
-	return 0;
-}
-#include <stdio.h>
-
-int main(void) {
 	printf("Nathan Liew \n");
-	printf("Computer Science")
+	printf("Computer Science");
 	return 0;
 }
